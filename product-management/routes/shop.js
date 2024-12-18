7c4a591578b02176d3c52d0e739165e5fe1e6f68
Man Nguyen <minhman2202@gmail.com>
2024-12-18 22:39:06 +0700

    rendering product detail view

--- product-management/routes/shop.js
+++ product-management/routes/shop.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
+
+// /products/:productId is a dynamic segment
+router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
