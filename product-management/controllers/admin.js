7b671659bbf330637375ac93fb6fec5ba9cf48d4
Man Nguyen <minhman2202@gmail.com>
2024-12-18 15:29:12 +0700

    renaming controllers & routes

--- product-management/controllers/admin.js
+++ product-management/controllers/admin.js
@@ -15,16 +15,3 @@ exports.postAddProduct = (req, res, next) => {
   product.save();
   res.redirect('/');
 };
-
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-  });
-};
--- product-management/routes/shop.js
+++ product-management/routes/shop.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getProducts);
+router.get('/products');
+router.get('/cart');
+router.get('/checkout');
 
 module.exports = router;
