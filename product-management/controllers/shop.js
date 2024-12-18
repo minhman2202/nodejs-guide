17c971c933e3bb1a1d6752c043000e59331a1336
Man Nguyen <minhman2202@gmail.com>
2024-12-18 16:23:38 +0700

    setting up navigation system for the app

--- product-management/controllers/shop.js
+++ product-management/controllers/shop.js
@@ -4,11 +4,39 @@ exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: 'All Products',
+      path: '/products'
     });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    pageTitle: 'Your Cart',
+    path: '/cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    pageTitle: 'Your Orders',
+    path: '/orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout',
+    path: '/checkout'
   });
 };
--- product-management/routes/shop.js
+++ product-management/routes/shop.js
@@ -4,9 +4,10 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', shopController.getProducts);
-router.get('/products');
-router.get('/cart');
-router.get('/checkout');
+router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
+router.get('/cart', shopController.getCart);
+router.get('/orders', shopController.getOrders);
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
