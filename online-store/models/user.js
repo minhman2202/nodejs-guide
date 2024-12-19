3ca0bfefff570995a0db7be851b889a03c7e2d29
Man Nguyen <minhman2202@gmail.com>
2024-12-19 23:09:15 +0700

    associating product to the user who creates it

--- online-store/models/user.js
+++ online-store/models/user.js
@@ -1,16 +1,36 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = require('../util/database');
+const getDb = require('../util/database').getDb;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    return getDb().collection('users')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(id) {
+    return getDb().collection('users')
+      .find({_id: new mongodb.ObjectId(id)})
+      .next()
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = User;
