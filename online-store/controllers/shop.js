6c83bb5f3f5f69da1bbc13fe65460afff37ece75
Man Nguyen <minhman2202@gmail.com>
2024-12-19 23:54:15 +0700

    adding product item to cart and show the cart

--- online-store/controllers/shop.js
+++ online-store/controllers/shop.js
@@ -38,9 +38,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.user.getCart().then(cart => {
-    return cart.getProducts();
-  }).then(products => {
+  req.user.getCart().then(products => {
     res.render('shop/cart', {
       path: '/cart',
       pageTitle: 'Your Cart',
@@ -53,28 +51,11 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  let fetchedCart;
-  let newQuantity = 1;
-
-  req.user.getCart().then(cart => {
-    fetchedCart = cart;
-    return cart.getProducts({where: {id: prodId}});
-  }).then(products => {
-    let product;
-    if (products.length > 0) {
-      product = products[0];
-    }
-    if (product) {
-      const oldQuantity = product.cartItem.quantity;
-      newQuantity = oldQuantity + 1;
-      return product;
-    }
-    return Product.findByPk(prodId)
-  }).then(product => {
-    return fetchedCart.addProduct(product, {through: {quantity: newQuantity}});
-  }).then(() => {
-      res.redirect('/cart');
-    }).catch(err => {
+  Product.findById(prodId).then(product => {
+    return req.user.addToCart(product);
+  }).then(result => {
+    res.redirect('/cart');
+  }).catch(err => {
     console.log(err);
   });
 };
--- online-store/models/user.js
+++ online-store/models/user.js
@@ -1,11 +1,14 @@
 const mongodb = require('mongodb');
+const {ObjectId} = require("mongodb");
 
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
@@ -16,6 +19,44 @@ class User {
       })
       .catch(err => {
         console.log(err);
+      });
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({productId: new ObjectId(product._id), quantity: newQuantity});
+    }
+
+    const updatedCart = {items: [...updatedCartItems]};
+    return getDb().collection('users')
+      .updateOne(
+        {_id: new ObjectId(this._id)},
+        {$set: {cart: updatedCart}}
+      );
+  }
+
+  getCart() {
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return getDb().collection('products')
+      .find({_id: {$in: productIds}})
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {...p, quantity: this.cart.items.find(i => {
+            return i.productId.toString() === p._id.toString();
+          }).quantity};
+        });
       });
   }
 
