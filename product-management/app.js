7f4cddbca295d0220f5486656b4705dda8e0e95c
Man Nguyen <minhman2202@gmail.com>
2024-12-19 13:03:36 +0700

    creating relationship between product & user models and associating them when storing data

--- product-management/app.js
+++ product-management/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,15 +19,37 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
-
-sequelize.sync().then(result => {
-  console.log(result);
-}).catch(err => {
-  console.log(err);
+app.use((req, res, next) => {
+  User.findByPk(1).then(user => {
+    // set a dummy user to context of every coming request
+    req.user = user;
+    next();
+  }).catch(err => {
+    console.log(err);
+  });
 });
 
-app.listen(3000);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(errorController.get404);
+
+// Define relationships between tables
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+// Create tables based on models
+sequelize.sync()
+  .then(result => {
+    // create a dummy user
+    User.findByPk(1).then(user => {
+      if (!user) {
+        return User.create({name: 'Kelvin', email: 'test@example.com'});
+      }
+      return Promise.resolve(user);
+    }).then(user => {
+      console.log(user);
+      app.listen(3000);
+    });
+  }).catch(err => {
+  console.log(err);
+});
