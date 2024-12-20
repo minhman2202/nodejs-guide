b02f30aaee7e716c21651d7f5a6253ca8e68b294
Man Nguyen <minhman2202@gmail.com>
2024-12-20 10:43:57 +0700

    adding & using user model

--- online-store/app.js
+++ online-store/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('67644285249b4d25c37445ac')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('6764e7c92a05c7b66d90d7f6')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,6 +33,14 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://kelvin:m33VFfqybmOYOP0J@cluster0.hkhdf.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Kelvin',
+          email: 'test@example.com'
+        }).save();
+      }
+    });
     app.listen(3000);
   }).catch(err => {
   console.log(err);
