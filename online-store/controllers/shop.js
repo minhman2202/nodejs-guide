06a5aad6d15b8e4430b0384ba41963e9570ac1b3
Man Nguyen <minhman2202@gmail.com>
2024-12-20 12:48:40 +0700

    clearing the cart after storing an order

--- online-store/controllers/shop.js
+++ online-store/controllers/shop.js
@@ -89,6 +89,8 @@ exports.postOrder = (req, res, next) => {
 
       return order.save();
     }).then(() => {
+    return req.user.clearCart();
+  }).then(() => {
     res.redirect('/orders');
   }).catch(err => {
     console.log(err);
