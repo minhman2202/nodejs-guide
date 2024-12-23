f738f070d4f712130fd62a503831663d59f5a884
Man Nguyen <minhman2202@gmail.com>
2024-12-23 10:37:26 +0700

    message-service: adding Post model and storing post in mongodb

--- message-service/controllers/feed.js
+++ message-service/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -28,15 +30,21 @@ exports.createPost = (req, res, next) => {
 
   const title = req.body.title;
   const content = req.body.content;
+
   // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {name: 'Kelvin'},
-      createdAt: new Date()
-    }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/duck.jpg',
+    creator: {name: 'Kelvin'}
+  });
+  post.save().then(result => {
+    console.log(result);
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: result
+    });
+  }).catch(err => {
+    console.log(err);
   });
 };
