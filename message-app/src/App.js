ffe09282d2308fdc69ecc2551b90a200b67730cc
Man Nguyen <minhman2202@gmail.com>
2024-12-23 13:47:58 +0700

    message-app: adding signup flow

--- message-app/src/App.js
+++ message-app/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   state = {
     showBackdrop: false,
     showMobileNav: false,
-    isAuth: true,
+    isAuth: false,
     token: null,
     userId: null,
     authLoading: false,
@@ -99,8 +99,18 @@ class App extends Component {
 
   signupHandler = (event, authData) => {
     event.preventDefault();
-    this.setState({ authLoading: true });
-    fetch('URL')
+    this.setState({authLoading: true});
+    fetch('http://localhost:8080/auth/signup', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: authData.signupForm.email.value,
+        password: authData.signupForm.password.value,
+        name: authData.signupForm.name.value
+      })
+    })
       .then(res => {
         if (res.status === 422) {
           throw new Error(
