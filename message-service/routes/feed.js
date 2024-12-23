346beab3e4fb37ba9a46b8bcdb090ad8d436d00c
Man Nguyen <minhman2202@gmail.com>
2024-12-23 12:58:16 +0700

    message-service: updating & deleting a post

--- message-service/routes/feed.js
+++ message-service/routes/feed.js
@@ -5,10 +5,10 @@ const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
-// GET /feed/posts
+// GET /feed/posts - fetching all posts
 router.get('/posts', feedController.getPosts);
 
-// POST /feed/post
+// POST /feed/post - creating a post
 router.post('/post', [ // [MMN] validate the input using validator middleware
   body('title')
     .trim()
@@ -18,7 +18,20 @@ router.post('/post', [ // [MMN] validate the input using validator middleware
     .isLength({min: 5})
 ], feedController.createPost);
 
-// GET /feed/post/:postId
+// GET /feed/post/:postId - fetching a single post
 router.get('/post/:postId', feedController.getPost);
+
+// PUT /feed/post/:postId - editing a post
+router.put('/post/:postId', [
+  body('title')
+    .trim()
+    .isLength({min: 5}),
+  body('content')
+    .trim()
+    .isLength({min: 5})
+], feedController.updatePost);
+
+// DELETE /feed/post/:postId - deleting a post
+router.delete('/post/:postId', feedController.deletePost);
 
 module.exports = router;
