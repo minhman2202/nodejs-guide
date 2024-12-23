573926cb6db0218d4a0a4eb22b539f3c5b30d866
Man Nguyen <minhman2202@gmail.com>
2024-12-23 14:43:23 +0700

    message-service: adding auth middleware to all routes

--- message-service/routes/feed.js
+++ message-service/routes/feed.js
@@ -2,27 +2,29 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // GET /feed/posts - fetching all posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post - creating a post
-router.post('/post', [ // [MMN] validate the input using validator middleware
-  body('title')
-    .trim()
-    .isLength({min: 5}),
-  body('content')
-    .trim()
-    .isLength({min: 5})
-], feedController.createPost);
+router.post('/post',isAuth,
+  [ // [MMN] validate the input using validator middleware
+    body('title')
+      .trim()
+      .isLength({min: 5}),
+    body('content')
+      .trim()
+      .isLength({min: 5})
+  ], feedController.createPost);
 
 // GET /feed/post/:postId - fetching a single post
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 // PUT /feed/post/:postId - editing a post
-router.put('/post/:postId', [
+router.put('/post/:postId', isAuth, [
   body('title')
     .trim()
     .isLength({min: 5}),
@@ -32,6 +34,6 @@ router.put('/post/:postId', [
 ], feedController.updatePost);
 
 // DELETE /feed/post/:postId - deleting a post
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
