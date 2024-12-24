437a32179edfb08c25d0bec39b1e66607436c04c
Man Nguyen <minhman2202@gmail.com>
2024-12-24 11:13:56 +0700

    message-graphql: adding a mutation for createUser

--- message-graphql/graphql/schema.js
+++ message-graphql/graphql/schema.js
@@ -10,7 +10,37 @@ module.exports = buildSchema(`
     hello: TestData
   }
   
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
+  }
+  
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String
+    status: String!
+    posts: [Post!]!
+  }
+  
+  input UserInput {
+    email: String!
+    name: String!
+    password: String!
+  }
+  
+  type RootMutation {
+    createUser(userInput: UserInput): User!
+  }
+  
   schema {
     query: RootQuery
+    mutation: RootMutation
   }
 `);
